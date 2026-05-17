7cdffcb5bc8a91953409bdc513a0f57993786d65
Aashu4you <ats4219511@gmail.com>
2026-05-17 10:34:59 +0530

    Register api working

--- server/server.js
+++ server/server.js
@@ -1,13 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("Farmify API Running");
